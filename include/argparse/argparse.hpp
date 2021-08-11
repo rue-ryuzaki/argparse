46c1a598632d70e60ecca96e101dbf8c92992cf2
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2021-08-11 21:14:46 +0300

    Version changed to v0.2.3

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Argument parser for C++11 (ArgumentParser v0.2.2)
+ * Argument parser for C++11 (ArgumentParser v0.2.3)
  *
  * Copyright (c) 2021 Golubchikov Mihail <https://github.com/rue-ryuzaki>
  *
@@ -29,7 +29,7 @@
 
 #define ARGPARSE_VERSION_MAJOR 0
 #define ARGPARSE_VERSION_MINOR 2
-#define ARGPARSE_VERSION_PATCH 2
+#define ARGPARSE_VERSION_PATCH 3
 #define ARGPARSE_VERSION_RC 0
 
 #include <algorithm>
