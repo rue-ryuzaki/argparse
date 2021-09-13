5502b5d94ec00240471b2271abce51f9fa7fc555
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2021-09-13 10:10:42 +0300

    Add '12. default values' test case

--- test/unit-argument_parser.cpp
+++ test/unit-argument_parser.cpp
@@ -1378,3 +1378,74 @@ TEST_CASE("11. subparsers", "[argument_parser]")
         REQUIRE(args4.get<std::string>("baz") == "Z");
     }
 }
+
+TEST_CASE("12. default values", "[argument_parser]")
+{
+    std::string global_default = "global";
+    std::string local_default = "local";
+    std::string new_default = "new";
+
+    SECTION("12.1. have global value") {
+        auto parser = argparse::ArgumentParser().argument_default(global_default).exit_on_error(false);
+
+        parser.add_argument("--foo").action("store").help("foo help");
+        parser.add_argument("--bar").action("store").default_value(local_default).help("bar help");
+
+        REQUIRE(parser.get_default("foo") == global_default);
+        REQUIRE(parser.get_default("bar") == local_default);
+
+        auto args0 = parser.parse_args({ });
+        REQUIRE(args0.get<std::string>("foo") == global_default);
+        REQUIRE(args0.get<std::string>("bar") == local_default);
+
+        parser.set_defaults({ { "foo", new_default } });
+        REQUIRE(parser.get_default("foo") == new_default);
+        REQUIRE(parser.get_default("bar") == local_default);
+
+        auto args1 = parser.parse_args({ });
+        REQUIRE(args1.get<std::string>("foo") == new_default);
+        REQUIRE(args1.get<std::string>("bar") == local_default);
+
+        parser.set_defaults({ { "foobaz", new_default } });
+        REQUIRE(parser.get_default("foo") == new_default);
+        REQUIRE(parser.get_default("bar") == local_default);
+        REQUIRE(parser.get_default("foobaz") == new_default);
+
+        auto args2 = parser.parse_args({ });
+        REQUIRE(args2.get<std::string>("foo") == new_default);
+        REQUIRE(args2.get<std::string>("bar") == local_default);
+        REQUIRE(args2.get<std::string>("foobaz") == new_default);
+    }
+
+    SECTION("12.2. no global value") {
+        auto parser = argparse::ArgumentParser().exit_on_error(false);
+
+        parser.add_argument("--foo").action("store").help("foo help");
+        parser.add_argument("--bar").action("store").default_value(local_default).help("bar help");
+
+        REQUIRE(parser.get_default("foo") == "");
+        REQUIRE(parser.get_default("bar") == local_default);
+
+        auto args0 = parser.parse_args({ });
+        REQUIRE(args0.get<std::string>("foo") == "");
+        REQUIRE(args0.get<std::string>("bar") == local_default);
+
+        parser.set_defaults({ { "foo", new_default } });
+        REQUIRE(parser.get_default("foo") == new_default);
+        REQUIRE(parser.get_default("bar") == local_default);
+
+        auto args1 = parser.parse_args({ });
+        REQUIRE(args1.get<std::string>("foo") == new_default);
+        REQUIRE(args1.get<std::string>("bar") == local_default);
+
+        parser.set_defaults({ { "foobaz", new_default } });
+        REQUIRE(parser.get_default("foo") == new_default);
+        REQUIRE(parser.get_default("bar") == local_default);
+        REQUIRE(parser.get_default("foobaz") == new_default);
+
+        auto args2 = parser.parse_args({ });
+        REQUIRE(args2.get<std::string>("foo") == new_default);
+        REQUIRE(args2.get<std::string>("bar") == local_default);
+        REQUIRE(args2.get<std::string>("foobaz") == new_default);
+    }
+}
