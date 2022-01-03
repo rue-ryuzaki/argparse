d2ee7d402090c45e1c8a1a81124abc97a457f161
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2022-01-03 22:01:12 +0300

    Update copyright

--- include/argparse/argparse.hpp
+++ include/argparse/argparse.hpp
@@ -3,7 +3,7 @@
  *
  * Argument parser for C++11 (ArgumentParser v1.1.2)
  *
- * Copyright (c) 2021 Golubchikov Mihail <https://github.com/rue-ryuzaki>
+ * Copyright (c) 2021-2022 Golubchikov Mihail <https://github.com/rue-ryuzaki>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
