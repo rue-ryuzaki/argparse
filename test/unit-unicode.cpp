159db21001c7a9cf1cee1a7cd1e8eca618b5d19e
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2023-04-17 18:43:41 +0300

    [test] update unicode to upper test case

--- test/unit-unicode.cpp
+++ test/unit-unicode.cpp
@@ -1460,7 +1460,7 @@ TEST_CASE("1. unicode to upper", "[detail]")
         table_map[0x1e941] = 0x1e91f;
         table_map[0x1e942] = 0x1e920;
         table_map[0x1e943] = 0x1e921;
-        for (uint32_t i = 0; i < 0xffff; ++i) {
+        for (uint32_t i = 0; i < 0x1e9ff; ++i) {
             if (table_map.count(i) == 0) {
                 REQUIRE(argparse::detail::_to_upper_codepoint(i) == i);
             }
