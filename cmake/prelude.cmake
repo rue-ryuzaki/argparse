eaa3c5d740571ed62d011f41e314d85bcf9ef723
Golubchikov Mihail <golubchikov.mihail@gmail.com>
2026-06-05 13:51:28 +0300

    [cmake] Update project files

--- cmake/prelude.cmake
+++ cmake/prelude.cmake
@@ -1,5 +1,5 @@
 # In-source guard
-if (CMAKE_SOURCE_DIR STREQUAL CMAKE_BINARY_DIR)
+if ("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
     message(
         FATAL_ERROR
         "In-source builds are not supported. "
